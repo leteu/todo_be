45c003ef7b5896307a8d9a32923e304e0aff0d58
ghshin <leteu610@gmail.com>
2023-07-25 17:12:31 +0900

    Fix: cors

--- app.ts
+++ app.ts
@@ -1,11 +1,14 @@
 import express from 'express'
+import http from 'http'
+import cors from 'cors'
+
 import { Client } from 'pg'
 import { router } from 'router'
 
 const port = 8080
 
 const app = express()
-app.use(express.json())
+const server = http.createServer(app)
 
 export const dbClient = new Client({
   user: 'postgres',
@@ -18,9 +21,11 @@ export const dbClient = new Client({
 ;(async () => {
   await dbClient.connect()
 
+  app.use(cors())
+  app.use(express.json())
   app.use('/', router)
 
-  app.listen(port, () => {
+  server.listen(port, () => {
     console.log(`
       Server listening on port: ${port}
     `);
