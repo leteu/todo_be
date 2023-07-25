7afecd146d7b8a81984f7c6b57259ec6b85bf27d
ghshin <leteu610@gmail.com>
2023-07-25 12:30:40 +0900

    Fix(todo): router url

--- src/router/todoRouter.ts
+++ src/router/todoRouter.ts
@@ -4,9 +4,9 @@ import { getTodoList, getTodoArchiveList, saveTodo, archiveTodo } from 'controll
 
 const router = Router()
 
-router.post('/todo/list', getTodoList)
-router.post('/todo/archive', getTodoArchiveList)
-router.post('/todo/save', saveTodo)
-router.delete('/todo/delete/:id', archiveTodo)
+router.post('/list', getTodoList)
+router.post('/archive', getTodoArchiveList)
+router.post('/save', saveTodo)
+router.delete('/delete/:id', archiveTodo)
 
 export { router as todoRouter }
