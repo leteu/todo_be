048514e2ec250df7937034c21189e028437a6b6f
ghshin <leteu610@gmail.com>
2023-07-25 17:26:15 +0900

    Fix: node link service 삭제된 todo 안나오게 수정

--- src/service/node.service.ts
+++ src/service/node.service.ts
@@ -3,11 +3,18 @@ import { dbClient } from '@/app'
 const getNodeLinkService = () => {
   return dbClient.query(
     `
-      SELECT t.tag_id AS "tagId", t.desc AS "tagDesc", 
+      SELECT 
+        t.tag_id AS "tagId", 
+        t.desc AS "tagDesc", 
         json_agg(json_build_object('id', todo_id, 'desc', td.desc)) AS todos
-      FROM tag t
-      LEFT JOIN todo td ON t.tag_id = td.tag_id
-      GROUP BY t.tag_id, t.desc;
+      FROM
+        tag t
+      LEFT JOIN 
+        todo td ON t.tag_id = td.tag_id
+      WHERE
+        td.del_dt IS NULL
+      GROUP BY 
+        t.tag_id, t.desc;
     `
   )
 }
