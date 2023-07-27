01198e113ce77df2f243577bf00d63a2288e4235
ghshin <leteu610@gmail.com>
2023-07-27 10:26:00 +0900

    Fix: todo list 태그 object 처리

--- src/service/todo.service.ts
+++ src/service/todo.service.ts
@@ -2,7 +2,31 @@ import { dbClient } from '@/app'
 
 const selectTodoService = (limit: number, page: number) => {
   return dbClient.query(
-    `SELECT * FROM todo WHERE "del_dt" IS NULL ORDER BY "todo_id" DESC LIMIT $1 OFFSET $2`,
+    `
+      SELECT
+        td.todo_id AS "id",
+        td."desc" AS "desc",
+        td.done AS "done",
+        td.reg_dt AS "regDt",
+        td.mod_dt AS "modDt",
+        td.del_dt AS "delDt",
+        jsonb_build_object('id', t.tag_id, 'desc', t.desc) AS tag
+      FROM
+        todo td
+      INNER JOIN
+        tag t ON td.tag_id = t.tag_id
+      WHERE
+        td.del_dt IS NULL
+      GROUP BY 
+        td.todo_id,
+        t.tag_id
+      ORDER by
+        td.todo_id desc
+      limit
+        $1
+      OFFSET
+        $2
+    `,
     [limit, page * limit]
   )
 }
